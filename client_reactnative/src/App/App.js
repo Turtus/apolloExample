eae7e1cee06dec42ee9b18c2146e3224e67e265b
ksu <ksu.zhytomirsky@gmail.com>
2021-11-25 14:02:01 +0200

    basic react native

---
+++ client_reactnative/src/App/App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {Node} from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+
+const App: () => Node = () => {
+
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>test</Text>
+            </View>
+        </SafeAreaView>
+    );
+};
+
+export default App;
